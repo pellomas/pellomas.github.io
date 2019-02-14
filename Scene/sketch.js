40214c08ba05e5272b2b255853c2bffc92b04d12
Lomas0402074 <0402074@sps-student.sk.ca>
2019-02-14 15:05:31 -0600

    Index & First project started
    14th of Febrary, 2019

---
+++ Scene/sketch.js
@@ -0,0 +1,17 @@
+// Project Title
+// Patrick Lomas
+// 2/14/2019
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  ellipse(mouseX , mouseY, 69)
+  
+}
