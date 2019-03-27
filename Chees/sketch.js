2223ee0be7ee1cca5bb901ab16c4401c1c316474
Lomas0402074 <0402074@sps-student.sk.ca>
2019-03-27 15:19:59 -0600

    perlin noise

--- Chees/sketch.js
+++ Chees/sketch.js
@@ -14,17 +14,25 @@ function setup() {
 function fiveMin(){
   for(i = 0; i <= 3; i++){
     rotate(6)
-    strokeWeight(5);
+    strokeWeight(height /100);
     line(width/5, 0, width/6, 0);
   }
   rotate(6);
-  strokeWeight(15);
+  strokeWeight(height / 50);
   line(width/5, 0, width/6, 0);
+}
+
+function windowResized(){
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   translate(width/2, height/2);
-  //for(i = 0; i <= 12; i++){
+  fill(255);
+  strokeWeight(height/200)
+  ellipse(0, 0, width/2.25);
+  for(e = 0; e <= 12; e++){
     fiveMin();
-  //}
+  }
+  ellipse(0, 0, height/ 2000);
 }
