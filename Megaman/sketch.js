e023778b40ccd4bcbac662db1359dcf0d05fc506
Patrick <misterVFD@gmail.com>
2019-03-30 16:39:23 -0600

    Fixed some bugs, added initial menu

--- Megaman/sketch.js
+++ Megaman/sketch.js
@@ -9,68 +9,74 @@
   
   let rush;
 
+  let button;
+
   //Variables for the Sniper Joe 
-  let sniperJoeHealth;
-  let enemyDamage;
-  let sniperJoeXPos;
-  let sniperJoeYPos;
 
   function setup() {
     document.addEventListener("contextmenu", event => event.preventDefault());
     createCanvas(windowWidth, windowHeight);
+    imageMode(CENTER);
     fill(0);
     initializeVariables();
     initializeMegamanVariables();
     initializeRushVariables();
-
-    sniperJoeHealth = 8;
-    sniperJoeXPos = windowWidth * 0.75;
-    sniperJoeYPos = windowHeight - megamanHeight;
-
+    button = {
+      width: 120*xScaler,
+      height: 200*yScaler,
+      xPosition: width/2,
+      yPosition: height/2,
+    };
+   
+    
   }
 
-  //Let's start to make some enemies--------------------------------------------------------------
-
-  function makeSniperJoe(joeX, joeY){
-    if (sniperJoeHealth > 0){
-      if (sniperJoeXPos < megamanXPos){
-        image(sniperJoeRight, sniperJoeXPos, sniperJoeYPos, megamanWidth, megamanHeight);
-      }
-      else{
-        image(sniperJoeLeft, sniperJoeXPos, sniperJoeYPos, megamanWidth, megamanHeight);
-      }
-      if (shot1XPosition && shot1yPosition){
-
-      }
-    }
-  }
-
+ 
   //Makes the player shoot when the mouse is clicked
-  function mousePressed(){  
-    if (mouseButton === LEFT){
-      if (!shot3Out && shot2Out && shot1Out){//check if shot 3 is out, then check shot 2, then shot 1
-        shot3Out = true;
+  function mousePressed(){ 
+    if (gameMode){ 
+      if (mouseButton === LEFT){
+        if (!shot3Out && shot2Out && shot1Out){//check if shot 3 is out, then check shot 2, then shot 1
+          shot3Out = true;
+        }
+        if (!shot2Out && shot1Out) {
+          shot2Out = true;
+        }
+        if (!shot1Out) {
+          shot1Out = true;
+        }
       }
-      if (!shot2Out && shot1Out) {
-        shot2Out = true;
+      if (mouseButton === RIGHT){
+        summonRush();
       }
-      if (!shot1Out) {
-        shot1Out = true;
-      }
-    }
-    if (mouseButton === RIGHT){
-      summonRush();
     }
   }
 
-  
+  function touchingRight(){
+    if (megamanXPos + megamanWidth/2 >= width){
+      megamanXSpeed = 0;
+      megamanXPos = width - megamanWidth/2;
+    }
+  }
+
+  function displayButtons(){
+    image(megamanMenuFace, button.xPosition, button.yPosition, button.width, button.height);
+    if (mouseIsPressed && mouseX > button.xPosition - button.width/2 && mouseX < button.xPosition + button.width/2 && mouseY > button.yPosition - button.height/2 && mouseY < button.yPosition + button.height/2){
+      gameMode = true;
+    }
+  }
   
   function draw() {
-    background(100);
-    if (gameMode){      
+    if(gameMode === false){
+      background(250);
+      displayButtons();
+    }
+    if (gameMode === true){ 
+      background(100);     
       moveX();
       moveStep();
       touchingLeft();
+      touchingRight();
       cleanUpStep();
       touchingBottom();
       shoot();
@@ -83,4 +89,11 @@
     createCanvas(windowWidth, windowHeight);
     xScaler = (windowWidth/900);
     yScaler = (windowHeight/900);
+    rush.height = yScaler * 180;
+    rush.width = xScaler * 85;
+    rush.speed = 25*yScaler;
+    rush.bounce = 20*yScaler;
+    megamanWidth = xScaler*85;
+    megamanHeight = yScaler*200;
+    megamanShotSpeed = xScaler * 10;
   }
